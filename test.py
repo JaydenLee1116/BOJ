c645acf7652629c503160fd2a43f880a99bdbea2
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-02-26 01:16:33 +0900

    16days

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
 import sys
-
-id = sys.stdin.readline().rstrip()
-
-print(id + '??!')
---
+++ 기초/[14681]사분면 고르기.py
@@ -0,0 +1,15 @@
+import sys
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+
+if A > 0:
+    if B > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if B > 0:
+        print(2)
+    else:
+        print(3)
