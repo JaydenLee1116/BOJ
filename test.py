014d862e7a329956738550ff2c2f4b47e1953f7a
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-03-04 23:50:48 +0900

    22days

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 import sys
+
+N = int(sys.stdin.readline())
+S = N
+
+count = 0
+
+while True:
+    N = (N % 10) * 10 + (N // 10 + N % 10) % 10
+    count += 1
+    if N == S:
+        print(count)
+        break
+    else:
+        continue
