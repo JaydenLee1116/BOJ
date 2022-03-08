abef1e3608da7376d64468c59a967c8525580d68
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-03-08 15:51:55 +0900

    25days

--- 기초/[11720]숫자의 합.py
+++ 기초/[11720]숫자의 합.py
@@ -2,11 +2,14 @@ import sys
 
 N = int(sys.stdin.readline())
 
-n = sys.stdin.readline()
+n = sys.stdin.readline().rstrip()
 
 tot = 0
 
-for i in range(N):
-    tot += int(n[i])
+# for i in range(N):
+#     tot += int(n[i])
+
+for i in n:
+    tot += int(i)
 
 print(tot)
