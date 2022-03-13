b4662dacd47aa3ed5ba8198a879893c6b90bc3d3
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-03-13 14:45:57 +0900

    27days

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
 import sys
 
-N = int(sys.stdin.readline())
-
-for _ in range(N):
-    noun = sys.stdin.readline().rstrip()
-    arr = []
-    temp = 0
-    
-    for alp in noun:
-        arr.append(alp)
-    
-    if len(arr) == 1:
-        continue
-    else:
-        for i in range(len(arr) - 1):
-            if arr[i] != arr[i+1]:
-                temp += 1
-        if temp >= len(set(arr)):
-            N -= 1
-print(N)
