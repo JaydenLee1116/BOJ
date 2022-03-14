5be6eee6c80424565d5a661b6d8b3d6bff9560d6
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-03-14 23:10:24 +0900

    28days

--- 기초/[2292]벌집.py
+++ 기초/[2292]벌집.py
@@ -11,3 +11,15 @@ else:
             break
         else:
             i += 1
+
+# 다른 풀이
+
+# n = int(input())
+
+# nums_pileup = 1  # 벌집의 개수, 1개부터 시작
+# cnt = 1
+
+# while n > nums_pileup :
+#     nums_pileup += 6 * cnt  # 벌집이 6의 배수로 증가
+#     cnt += 1
+# print(cnt)
