09179f2b369a45715740c5bf29a0e133ba30de73
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-03-16 20:36:38 +0900

    30days

--- 기초/[1193]분수찾기.py
+++ 기초/[1193]분수찾기.py
@@ -2,18 +2,21 @@ import sys
 
 X = int(sys.stdin.readline())
 
-line = 0  # 사선 라인
-max_num = 0  # 입력된 숫자(X 변수)의 라인에서 가장 큰 숫자
+line = 0  # 대각선 라인
+max_num = 0  # 라인에서 가장 큰 수
 while X > max_num:
     line += 1  
     max_num += line  
 
 gap = max_num - X 
-if line % 2 == 0:  # 사선 라인이 짝수번째 일 때
-    top = line - gap  #분자
-    under = gap + 1  #분모
-else :  # 사선 라인이 홀수번째 일 때
-    top = gap + 1  #분자
-    under = line - gap  #분모
+
+# 홀수일 때
+top = gap + 1  
+under = line - gap  
+
+# 짝수일 때
+if line % 2 == 0: 
+    top = line - gap  
+    under = gap + 1  
 
 print(f'{top}/{under}')
